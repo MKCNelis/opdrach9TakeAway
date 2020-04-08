0754796d5b11e8fbd574c0243a655ddb6fe70ea9
mkcnelis <maikel.nelis@gmail.com>
2020-04-08 09:27:32 +0200

    Update sushi.php

--- sushi.php
+++ sushi.php
@@ -1,56 +1,3 @@
-<?php
-    //function to save the array the 'winkelwagen.json' file
-    function SaveArray($p_aSaveArray) {
-        //change string into json compatible data
-        $aJSONArray = json_encode($p_aSaveArray);
-        //open the file in writing mode
-        $file = fopen('winkelwagen.json','w');
-        //change the files content of the opened file to what it already was + the new array
-        file_put_contents('winkelwagen.json', $aJSONArray);
-        //close the file
-        fclose($file);
-    }
-
-    //function to load the 'winkelwagen.json' file
-    function LoadArray() {
-        //open the file in reading mode
-        $file = fopen('winkelwagen.json','r');
-        //get the content of the opened file
-        $aJSONArray = file_get_contents('winkelwagen.json');
-        //change the read string to php compatible data
-        $aReadArray = json_decode($aJSONArray,TRUE);
-        //close the file
-        fclose($file);
-        //save the loaded data to be used in the page
-        return($aReadArray);
-    }
-
-    //function to clear the 'winkelwagen.json' file
-    function ClearArray() {
-        //open the file in writing mode
-        $file = fopen('winkelwagen.json','r');
-        //clear the file
-        file_put_contents('');
-        //run the function to reload the page
-        PlaceOrder();
-    }
-
-    if(!empty($_POST)){
-        $sProductNaam   = $_POST['sProductNaam'];
-        $fPrijs         = $_POST['fPrijs'];
-        $iAantal        = $_POST['iAantal'];
-        
-        $aWinkelwagen = LoadArray();
-        $iRecordCounter = count($aWinkelwagen);
-        $aWinkelwagen[$iRecordCounter] = array($sProductNaam,$fPrijs,$iAantal);
-        //save the array to the file
-        SaveArray($aWinkelwagen);
-        // header('location: sushi.php');
-        echo('<script>ReloadPage()</script>');
-    }
-    $iTotaal = 0;
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -64,31 +11,17 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="./css/Style.css">
-    <script>
-        function ReloadPage() {
-            document.getElementById('bConfirmation').innerHTML = '<p>are you sure this is what you want to buy?</p>\
-                <button id="bTrueConfirm"></button>\
-                <button id="bDone"></button>\
-                <button id="bCancel"></button>';
-            document.getElementById('bConfirmation').htmlstyle
-        }
-        
-    function PlaceOrder() {
-        document.reload();
-    }
-    </script>
 </head>
 <body>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup"></div>
-        <a class="navbar-brand" href="#"><img src="./afbeeldingen/noodlebowl.gif" height="50px"></a>
+        <a class="navbar-brand" href="#"><img src="../afbeeldingen/noodlebowl.gif" height="50px"></a>
        <div class="container_Big" style="padding-left:60%;">
          <button>login</button>
         <button>registreren</button>
-        <button> <img src="./afbeeldingen/street-food-cart.png" height="50px"></button>
+        <button> <img src="../afbeeldingen/street-food-cart.png" height="50px"></button>
         </div>
         </div>
      
-    <!-- <img src="/afbeeldingen/pusheen.gif" alt="cat"> -->
     <div class="container_Big">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#"></a>
@@ -100,99 +33,82 @@
             <a class="nav-item nav-link active" href="index.html">Home <span class="sr-only">(current)</span></a>
             <a class="nav-item nav-link" href="#">Hotpot</a>
             <a class="nav-item nav-link" href="#">Sushi</a>
-            <a class="nav-item nav-link" href="noodles.php">noodels</a>
-            <a class="nav-item nav-link" href="Hotpot.php">Hotpot</a>
+            <a class="nav-item nav-link" href="noodeles.html">noodels</a>
+            <a class="nav-item nav-link" href="#">Hotpot</a>
           </div>
         </div>
       </nav>
+    
+    <center>
+    <div class="backgound-dragon">
+      <hr>
+      <div class="row" style="padding: 5%;">
+        <div class="col-6" style="background-color: rgba(0, 0, 0, 0.329); height: 90%;">
+          <br>
+          <h><b>Norimaki</b></h><br>
+          <img src="./afbeeldingen/Sushi 1 (1).jpg" width="50%">
+          <form method="POST">
 
-    <div class="container_Big" style="background-color: var(--ColorGray);">
+            <input type="hidden" name="sProductNaam" value="sushi 1"><br>
+            <input type="hidden" name="fPrijs" value="3.52"><br>
+            <input type="number" name="iAantal">
+            <button type="submit" onclick="ReloadPage()">add to kart</button>
+            
+          </form>
+        </div>
 
-        <div class="container">
-            D
+        <div class="col-6" style="background-color: rgba(0, 0, 0, 0.329); height: 90%;">
+          <br>
+          <h><b>Unagi Kabayaki</b></h><br>
+          <img src="./afbeeldingen/Sushi 1 (2).jpg" width="50%">
+
+          <form method="POST">
+
+            <input type="hidden" name="sProductNaam" value="sushi 1"><br>
+            <input type="hidden" name="fPrijs" value="3.52"><br>
+            <input type="number" name="iAantal">
+            <button type="submit" onclick="ReloadPage()">add to kart</button>
+            
+            </form>
         </div>
-        <div class="container aligner" style="background-color: var(--ColorGray);">
-                <img src="./Afbeeldingen/Sushi-1.jfif" alt="doet nie" width="34%" height="30%" class="roundimage" border="5">
+      </div>
+
+      <hr>
+
+      <div class="row" style="padding: 5%;">
+        <div class="col-6" style="background-color: rgba(0, 0, 0, 0.329); height: 90%;">
+          <br>
+          <h><b>Sake rol</b></h><br>
+          <img src="./afbeeldingen/Sushi 1 (3).jpg" width="50%">
+          <form method="POST">
+
+            <input type="hidden" name="sProductNaam" value="sushi 1"><br>
+            <input type="hidden" name="fPrijs" value="3.52"><br>
+            <input type="number" name="iAantal">
+            <button type="submit" onclick="ReloadPage()">add to kart</button>
+            
+          </form>
         </div>
-        <hr>
-        <div class="container alignerboxes" style="background-color: var(--ColorGray);">
-            <div class="row" >
-                <div class="col-6">
-                    <form method="POST">
-                        <div>              
-                            <div class="ar-image">
-                                <div class="article-image">
-                                    <p>Lire plus<br>
-                                        <input type="hidden" name="sProductNaam" value="sushi 1"><br>
-                                        <input type="hidden" name="fPrijs" value="3.52"><br>
-                                        <input type="number" name="iAantal" required>
-                                        <button type="submit" onclick="ReloadPage()">add to kart</button>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-6">
-                    <form method="POST">
-                        <div>              
-                            <div class="ar-image">
-                                <div class="article-image">
-                                    <p>Lire plus<br>
-                                        <input type="hidden" name="sProductNaam" value="sushi 2"><br>
-                                        <input type="hidden" name="fPrijs" value="3.52"><br>
-                                        <input type="number" name="iAantal" required>
-                                        <button type="submit" onclick="ReloadPage()">add to kart</button>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="row" >
-                <div class="col-6">
-                    <form method="POST">
-                        <div>              
-                            <div class="ar-image">
-                                <div class="article-image">
-                                    <p>Lire plus<br>
-                                        <input type="hidden" name="sProductNaam" value="sushi 3"><br>
-                                        <input type="hidden" name="fPrijs" value="3.52"><br>
-                                        <input type="number" name="iAantal" required>
-                                        <button type="submit" onclick="ReloadPage()">add to kart</button>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-6">
-                    <form method="POST">
-                        <div>              
-                            <div class="ar-image">
-                                <div class="article-image">
-                                    <p>Lire plus<br>
-                                        <input type="hidden" name="sProductNaam" value="sushi 4"><br>
-                                        <input type="hidden" name="fPrijs" value="3.52"><br>
-                                        <input type="number" name="iAantal" required>
-                                        <button type="submit" onclick="ReloadPage()">add to kart</button>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+
+        <div class="col-6" style="background-color: rgba(0, 0, 0, 0.329); height: 90%;">
+          <br>
+          <h><b>Sushimet zalm</b></h><br>
+          <img src="./afbeeldingen/Sushi 1 (4).jpg" width="50%">
+          <form method="POST">
+
+            <input type="hidden" name="sProductNaam" value="sushi 1"><br>
+            <input type="hidden" name="fPrijs" value="3.52"><br>
+            <input type="number" name="iAantal">
+            <button type="submit" onclick="ReloadPage()">add to kart</button>
+            
+            </form>
         </div>
-        <div id="bConfirmation"></div>
+      </div>
+      <hr>
+    </div>
+    </center>
+    <hr>
+</div>
 
 </body>
-
-<script>
-        //function to reload the page
-        function PlaceOrder() {
-            window.location.replace("winkelwagen.php");
-        }
-    </script>
 </html>
